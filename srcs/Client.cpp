d1963325b18c9a8704ac9ccf8af135ba1b775b4f
pgeeser <pgeeser@student.42heilbronn.de>
2023-06-05 18:43:16 +0200

    many changes

--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -6,7 +6,7 @@
 /*   By: pgeeser <pgeeser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 11:13:06 by pgeeser           #+#    #+#             */
-/*   Updated: 2023/06/01 17:23:31 by pgeeser          ###   ########.fr       */
+/*   Updated: 2023/06/05 18:43:07 by pgeeser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,42 +18,83 @@
 /* -------------------------------------------------------------------------- */
 
 Client::Client(int fd) : fd(fd) {
-	std::cout << "Client created" << std::endl;
+	//@todo debug print
+	// std::cout << "Client created" << std::endl;
 }
 
 Client::~Client() {
-	std::cout << "Client destroyed" << std::endl;
+	//@todo debug print
+	// std::cout << "Client destroyed" << std::endl;
 }
 
 /* -------------------------------------------------------------------------- */
 /*                               Public Methods                               */
 /* -------------------------------------------------------------------------- */
 
-std::string	Client::recieveMessage(void) {
+/**
+ * The function receives data from a client and handles exceptions for different scenarios.
+ */
+void	Client::recieveData(void) {
 	static const int	bufferSize = 512;
 	char				buffer[bufferSize];
-	memset(buffer, 0, bufferSize);
+	// memset(buffer, 0, bufferSize); //@todo idk if allowed?
 	int ret = recv(this->fd, buffer, bufferSize, 0);
 	std::string msg(buffer);
 	if (msg.length() > 0)
-		std::cout << "[Client -> Server]: " << msg;
+		std::cout << "[Client(" << this->fd << ") -> Server]: " << msg;
 	if (ret == bufferSize) {
+		this->recieveBuffer.clear();
 		throw MessageTooLongException();
 	} else if (ret < 0) {
+		this->recieveBuffer.clear();
 		throw ConnectionErrorExcpetion();
 	} else if (ret == 0) {
+		this->recieveBuffer.clear();
 		throw ConnectionClosedException();
 	}
+	this->recieveBuffer += msg;
+}
+
+/**
+ * The function reads and returns the received data from a client, clearing the receive buffer if a
+ * complete message is found.
+ * 
+ * @return If the `recieveBuffer` contains a complete message (i.e. ends with "\r\n"), then the
+ * function returns the message as a `std::string` and clears the `recieveBuffer`. If the
+ * `recieveBuffer` does not contain a complete message, then an empty string is returned.
+ */
+std::string	Client::readRecievedData(void) {
+	std::string msg = this->recieveBuffer;
+	if (msg.length() > 0 && msg.find("\r\n") != std::string::npos)
+		this->recieveBuffer.clear();
+	else
+		return ("");
 	return (msg);
 }
 
-void		Client::sendMessage(std::string message) {
-	int ret = send(this->fd, message.c_str(), message.length(), 0);
+/**
+ * This function adds a string of data to the send buffer of a client object.
+ * 
+ * @param data The parameter "data" is a string variable that represents the data that needs to be
+ * added to the send buffer.
+ */
+void	Client::addDataToBuffer(std::string data) {
+	this->sendBuffer += data;
+}
+
+/**
+ * This function sends a message from the server to a client and prints it to the console.
+ * 
+ * @return The function does not have a return type specified, so it does not return anything.
+ */
+void		Client::sendMessage(void) {
+	int ret = send(this->fd, this->sendBuffer.c_str(), this->sendBuffer.length(), 0);
 	if (ret < 0) {
 		std::cout << "send error" << std::endl;
 		return ;
 	} else
-	std::cout << "[Server -> Client]: " << message << std::endl;
+	std::cout << "[Server -> Client(" << this->fd << ")]: " << this->sendBuffer << std::endl;
+	this->sendBuffer.clear();
 }
 
 /* -------------------------------------------------------------------------- */
--- srcs/Server.hpp
+++ srcs/Server.hpp
@@ -6,16 +6,19 @@
 /*   By: pgeeser <pgeeser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 11:13:20 by pgeeser           #+#    #+#             */
-/*   Updated: 2023/06/04 20:51:15 by pgeeser          ###   ########.fr       */
+/*   Updated: 2023/06/05 15:58:45 by pgeeser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+#include <poll.h>		// poll
 #include <map>			// std::map
 #include <netinet/in.h>	// sockaddr_in
 #include "Client.hpp"	// Client
 #include "Channel.hpp"	// Channel
+
+#define MAX_FDS 1000
 
 class Server {
 	public:
@@ -31,14 +34,14 @@ class Server {
 		void				removeChannel(Channel *channel);
 
 	private:
-		int 					port;
-		std::string				password;
-		int 					socketFd;
-		struct sockaddr_in		address;
-		fd_set					reads;
-		fd_set					writes;
-		std::map<int, Client*>	connectedClients;
-		std::vector<Channel*>	channels;
+		int 						port;
+		std::string					password;
+		int 						socketFd;
+		struct sockaddr_in			address;
+		std::vector<struct pollfd>	fds;
+		std::map<int, Client*>		connectedClients;
+		std::vector<Channel*>		channels;
 
 		void						acceptNewConnection(int fd);
+		void						recieveData(int fd);
 };
