bb3e4a256908aa7cb0f821996f40459acb7aa2ee
pgeeser <pgeeser@student.42heilbronn.de>
2023-06-06 15:04:40 +0200

    bad changes

--- srcs/Executer.cpp
+++ srcs/Executer.cpp
@@ -2,72 +2,26 @@
 #include "irc.hpp"
 #include "Message.hpp"
 #include "cmds/Commands.hpp"
+#include <vector>
 
 #include <iostream>
 
-typedef void (*cmdFunc)(Message& message, Server* server, Client* client);
-
-static void	cmdCAP(Message& message, Server* server, Client* client)
-{
-	CAP().execute(message, server, client);
-}
-
-static void	cmdPING(Message& message, Server* server, Client* client)
-{
-	PING().execute(message, server, client);
-}
-
-static void	cmdPASS(Message& message, Server* server, Client* client)
-{
-	PASS().execute(message, server, client);
-}
-
-static void	cmdNICK(Message& message, Server* server, Client* client)
-{
-	NICK().execute(message, server, client);
-}
-
-static void	cmdUSER(Message& message, Server* server, Client* client)
-{
-	USER().execute(message, server, client);
-}
-
-static void	cmdQUIT(Message& message, Server* server, Client* client)
-{
-	QUIT().execute(message, server, client);
-}
-
-static void	cmdPRIVMSG(Message& message, Server* server, Client* client)
-{
-	PRIVMSG().execute(message, server, client);
-}
-
-static void	cmdJOIN(Message& message, Server* server, Client* client)
-{
-	JOIN().execute(message, server, client);
-}
-
-static void	cmdPART(Message& message, Server* server, Client* client)
-{
-	PART().execute(message, server, client);
-}
-
-static const cmdFunc exec[] = {
-	cmdCAP,
-	cmdPING,
-	cmdPASS,
-	cmdNICK,
-	cmdUSER,
-	cmdQUIT,
-	cmdPRIVMSG,
-	cmdJOIN,
-	cmdPART
+std::vector<Command*> exec = {
+	&CAP(),
+	&PING(),
+	&PASS(),
+	&NICK(),
+	&USER(),
+	&QUIT(),
+	&PRIVMSG(),
+	&JOIN(),
+	&PART()
 };
 
 int	irc::executeMessage(Message& message, Server* server, Client* client)
 {
 	if (message.getId() < 0 || message.getId() > 7)
 		return (ERROR);
-	exec[message.getId()](message, server, client);
+	exec[message.getId()]->execute(message, server, client);
 	return (SUCCESS);
 }
