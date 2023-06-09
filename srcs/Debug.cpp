4ee8ca8f7208cd6daa66f86f534c2beac1222cf2
lorbke <lorbke@student.42heilbronn.de>
2023-06-09 20:33:36 +0200

    add is_trailing_bool

--- srcs/Debug.cpp
+++ srcs/Debug.cpp
@@ -13,5 +13,6 @@ void	debug::printMessage(const Message& command)
 	for (std::vector<std::string>::const_iterator it = command.getParams().begin(); it != command.getParams().end(); it++)
 		std::cout << "	[" << *it << "]" << std::endl;
 	std::cout << "trailing: " << command.getTrailing() << std::endl;
+	std::cout << "is trailing empty: " << command.isTrailingEmpty() << std::endl;
 	std::cout << std::endl;
 }
